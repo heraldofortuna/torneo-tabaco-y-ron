71d99f9d1f1f48b35f7764970f04845fae7e007f
Heraldo Fortuna <heraldofortuna@gmail.com>
2025-06-19 16:21:04 -0500

    se corrigieron las rutas del programa para que coincida con el nuevo hash impuesto por github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://heraldofortuna.github.io',
-  base: process.env.NODE_ENV === 'production' ? '/torneo-tabaco-y-ron/' : '/',
+  base: '/torneo-tabaco-y-ron',
   vite: {
     plugins: [tailwindcss()],
   },
