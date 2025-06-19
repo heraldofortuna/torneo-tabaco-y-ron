8505564af94f506863af5401447728bf4b749edb
Heraldo Fortuna <heraldofortuna@gmail.com>
2025-06-19 15:51:19 -0500

    se configuró la aplicación astro para poder deployarse dinámicamente en githubs pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://heraldofortuna.github.io',
+  base: 'torneo-tabaco-y-ron',
   vite: {
     plugins: [tailwindcss()],
   },
