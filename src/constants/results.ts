04fc8aa033460ec04a6bfa5d35c465a045b652db
Heraldo Fortuna <proveedor381@culqi.com>
2025-06-26 19:34:16 -0500

    se añadió la info de la segunda fecha

--- src/constants/results.ts
+++ src/constants/results.ts
@@ -7,40 +7,83 @@ export const results: ResultsData  = [
     date: 'Jugado el 18 de Junio del 2025',
     matches: [
       {
-        home: 'Rojo',
-        away: 'Real Coholicos',
+        home: 'Red Devils FC',
+        away: 'Realcoholicos FC',
         homeScorers: ['Josue', 'Frankito'],
         awayScorers: ['Sebastian'],
       },
       {
-        home: 'Azul',
-        away: 'Rojo',
+        home: 'FC Real Polaco',
+        away: 'Red Devils FC',
         homeScorers: ['Charly', 'Rodrigo', 'Rodrigo', 'Liam', 'Rodrigo', 'Jozua', 'Liam', 'Charly'],
         awayScorers: ['Josue', 'Kanuto', 'Josue', 'Frankito', 'Josue', 'Wachi', 'Kanuto'],
       },
       {
-        home: 'Real Coholicos',
-        away: 'Azul',
+        home: 'Realcoholicos FC',
+        away: 'FC Real Polaco',
         homeScorers: ['Omar', 'Omar', 'Pedro'],
         awayScorers: ['Rodrigo', 'Jozua', 'Rodrigo', 'Charly', 'Jozua'],
       },
       {
-        home: 'Real Coholicos',
-        away: 'Rojo',
+        home: 'Realcoholicos FC',
+        away: 'Red Devils FC',
         homeScorers: ['Omar', 'Omar', 'Russo', 'Pedro', 'Russo', 'Omar'],
         awayScorers: ['Frankito', 'Josue'],
       },
       {
-        home: 'Rojo',
-        away: 'Azul',
+        home: 'Red Devils FC',
+        away: 'FC Real Polaco',
         homeScorers: ['Frankito', 'Frankito', 'Frankito', 'Frankito', 'Frankito', 'Frankito'],
         awayScorers: ['Liam', 'Charly'],
       },
       {
-        home: 'Azul',
-        away: 'Real Coholicos',
+        home: 'FC Real Polaco',
+        away: 'Realcoholicos FC',
         homeScorers: ['Rodrigo', 'Rodrigo', 'Jozua'],
         awayScorers: ['Russo', 'Omar', 'Russo', 'Lucho', 'Russo'],
+      }
+    ],
+  },
+  {
+    id: 2,
+    name: 'Fecha 2',
+    date: 'Jugado el 25 de Junio del 2025',
+    matches: [
+      {
+        home: 'Realcoholicos FC',
+        away: 'FC Real Polaco',
+        homeScorers: ['Russo', 'Russo', 'Russo'],
+        awayScorers: [],
+      },
+      {
+        home: 'Realcoholicos FC',
+        away: 'Red Devils FC',
+        homeScorers: ['Russo', 'Russo', 'Russo', 'Omar'],
+        awayScorers: ['Kanuto', 'Kanuto', 'Kanuto', 'Frankito'],
+      },
+      {
+        home: 'FC Real Polaco',
+        away: 'Red Devils FC',
+        homeScorers: ['Jozua', 'Jozua', 'Charly'],
+        awayScorers: ['Frankito', 'Josue', 'Kanuto'],
+      },
+      {
+        home: 'FC Real Polaco',
+        away: 'Realcoholicos FC',
+        homeScorers: ['Charly', 'Charly'],
+        awayScorers: ['Russo', 'Omar', 'Omar'],
+      },
+      {
+        home: 'Red Devils FC',
+        away: 'Realcoholicos FC',
+        homeScorers: ['Kanuto', 'Kanuto', 'Wachi', 'Frankito'],
+        awayScorers: ['Omar', 'Russo'],
+      },
+      {
+        home: 'Red Devils FC',
+        away: 'FC Real Polaco',
+        homeScorers: ['Wachi', 'Wincho', 'Wincho'],
+        awayScorers: ['Charly', 'Jozua'],
       }
     ],
   }
